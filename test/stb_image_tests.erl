771e5dbaf16ac2edccbfa01c92e7c9f1897c37f6
Eddie Antonio Santos <easantos@ualberta.ca>
2015-09-07 11:51:24 -0600

    Add basic tests.

--- test/stb_image_tests.erl
+++ test/stb_image_tests.erl
@@ -2,9 +2,46 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-basic_test() ->
-    ?assertException(error, badarg, stb_image:load(foo)).
-
+% Test loading any regular image.
 load_test() ->
-    {ok, Image} = stb_image:load("/Users/eddieantonio/good.jpg"),
-    ?assert(is_binary(Image)).
+    Filename = filename_for(any_image),
+    ?assertMatch({ok, _Image}, stb_image:load(Filename)).
+
+% Test accessing a non-existent file.
+enoent_test() ->
+    Filename = filename_for(nonexistent, false),
+    ?assertMatch({error, enoent}, stb_image:load(Filename)).
+
+% Test reading a file with insufficient permissions.
+eacces_test() ->
+    {setup,
+     fun () -> file:change_mode(filename_for(untouchable), 8#044) end,
+     fun (_) -> file:change_mode(filename_for(untouchable), 8#644) end,
+     fun () ->
+             Filename = filename_for(untouchable),
+             ?assertMatch({error, eacces}, stb_image:load(Filename))
+     end
+    }.
+
+%% Utils.
+
+filename_for(Name) -> filename_for(Name, true).
+filename_for(Name, Exists) ->
+    BaseName = basename_for(Name),
+    Path = filename:join(image_dir(), BaseName),
+    io:format("Looking for file ~s~n", [Path]),
+    Exists = filelib:is_file(Path),
+    Path.
+
+image_dir() ->
+    case code:get_object_code(?MODULE) of
+        error ->
+            exit("Cannot find code object...");
+        {_, _, ModPath} ->
+            AppPath = filename:dirname(filename:dirname(ModPath)),
+            filename:join(AppPath, "priv")
+    end.
+
+basename_for(any_image) -> "1px_256_table.png";
+basename_for(nonexistent) -> "non-existent.png";
+basename_for(untouchable) -> "hammer.gif".
